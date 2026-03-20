392482895e93a1114b39055b251ae5a79cd04d4c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-20 11:10:01 -0300

    feat: add pt-BR translations
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ Resources/lang/pt-BR/general.php
@@ -0,0 +1,31 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+return [
+    'name'                  => 'NFS-e',
+    'saved'                 => 'Configurações salvas com sucesso.',
+    'certificate_uploaded'  => 'Certificado enviado e senha armazenada com segurança.',
+    'certificate_deleted'   => 'Certificado removido com sucesso.',
+    'invalid_pfx'           => 'Arquivo PFX inválido ou senha incorreta.',
+    'nfse_emitted'          => 'NFS-e :number emitida com sucesso.',
+    'nfse_cancelled'        => 'NFS-e cancelada com sucesso.',
+    'cancel_motivo_default' => 'Cancelamento solicitado pelo prestador de serviço.',
+    'service_default'       => 'Prestação de serviços conforme contrato.',
+
+    'settings' => [
+        'title'            => 'Configurações NFS-e',
+        'cnpj_prestador'   => 'CNPJ do Prestador',
+        'municipio_ibge'   => 'Código IBGE do Município',
+        'sandbox_mode'     => 'Modo Sandbox (Homologação)',
+        'bao_addr'         => 'Endereço OpenBao / Vault',
+        'bao_mount'        => 'Mount KV v2',
+        'bao_role_id'      => 'AppRole Role ID',
+        'bao_secret_id'    => 'AppRole Secret ID',
+        'certificate'      => 'Certificado ICP-Brasil (PFX)',
+        'pfx_password'     => 'Senha do Certificado',
+        'item_lista'       => 'Item da Lista de Serviços (LC 116)',
+        'aliquota'         => 'Alíquota ISS (%)',
+    ],
+];
