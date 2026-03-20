530e40d07b4f4ace3d513738aa239559fcab574b
Vitor Mattos <vitor@php.rio>
2026-03-20 16:19:08 -0300

    feat(menu): show NFS-e in settings menu
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ Listeners/ShowInSettingsMenu.php
@@ -0,0 +1,40 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Modules\Nfse\Listeners;
+
+use App\Events\Menu\SettingsCreated as Event;
+use App\Traits\Modules;
+use App\Traits\Permissions;
+
+class ShowInSettingsMenu
+{
+    use Modules;
+    use Permissions;
+
+    public function handle(Event $event): void
+    {
+        if (!$this->moduleIsEnabled('nfse')) {
+            return;
+        }
+
+        $title = trans('nfse::general.name');
+
+        if ($this->canAccessMenuItem($title, 'read-nfse-settings')) {
+            $event->menu->route(
+                'nfse.settings.edit',
+                $title,
+                [],
+                260,
+                [
+                    'icon' => 'receipt_long',
+                    'search_keywords' => trans('nfse::general.description'),
+                ]
+            );
+        }
+    }
+}
