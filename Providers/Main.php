7a31af36e0206f626eeb4e10325861966a8a45dc
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-20 11:09:54 -0300

    feat: add Main service provider
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ Providers/Main.php
@@ -0,0 +1,55 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Modules\Nfse\Providers;
+
+use Illuminate\Support\ServiceProvider as Provider;
+
+class Main extends Provider
+{
+    /**
+     * Boot the application events.
+     */
+    public function boot(): void
+    {
+        $this->loadTranslations();
+        $this->loadViews();
+        $this->loadMigrations();
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register(): void
+    {
+        $this->loadRoutes();
+    }
+
+    protected function loadViews(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'nfse');
+    }
+
+    protected function loadTranslations(): void
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'nfse');
+    }
+
+    protected function loadMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+
+    protected function loadRoutes(): void
+    {
+        if (app()->routesAreCached()) {
+            return;
+        }
+
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/admin.php');
+    }
+}
