f8591362c439ef0ff96af3318d4502eeb25cfbc4
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-20 11:09:54 -0300

    feat: add SettingsController
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ Http/Controllers/SettingsController.php
@@ -0,0 +1,45 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Modules\Nfse\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use LibreCodeCoop\NfsePHP\SecretStore\OpenBaoSecretStore;
+
+class SettingsController extends Controller
+{
+    public function edit(): \Illuminate\View\View
+    {
+        $settings = setting('nfse');
+
+        return view('nfse::settings.edit', compact('settings'));
+    }
+
+    public function update(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'nfse.cnpj_prestador'  => 'required|string|size:14',
+            'nfse.municipio_ibge'  => 'required|string|size:7',
+            'nfse.sandbox_mode'    => 'nullable|boolean',
+            'nfse.bao_addr'        => 'required|url',
+            'nfse.bao_mount'       => 'required|string',
+            'nfse.bao_role_id'     => 'nullable|string',
+            'nfse.bao_secret_id'   => 'nullable|string',
+        ]);
+
+        foreach ($request->input('nfse', []) as $key => $value) {
+            setting(['nfse.' . $key => $value]);
+        }
+
+        setting()->save();
+
+        return redirect()->route('nfse.settings.edit')
+            ->with('success', trans('nfse::general.saved'));
+    }
+}
