c6590f8a182a73ace4a8a77c37689af1df67acf9
Vitor Mattos <vitor@php.rio>
2026-03-20 16:19:08 -0300

    test(menu): cover settings menu listener behavior
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ tests/Unit/Listeners/ShowInSettingsMenuTest.php
@@ -0,0 +1,144 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace {
+    function trans(string $key): string
+    {
+        return $key;
+    }
+}
+
+namespace App\Traits {
+    trait Modules
+    {
+        public function moduleIsEnabled(string $alias): bool
+        {
+            return true;
+        }
+    }
+
+    trait Permissions
+    {
+        public function canAccessMenuItem(string $title, string $permission): bool
+        {
+            return true;
+        }
+    }
+}
+
+namespace App\Events\Menu {
+    class SettingsCreated
+    {
+        public function __construct(public object $menu)
+        {
+        }
+    }
+}
+
+namespace Modules\Nfse\Tests\Unit\Listeners {
+    use App\Events\Menu\SettingsCreated;
+    use Modules\Nfse\Listeners\ShowInSettingsMenu;
+    use Modules\Nfse\Tests\TestCase;
+
+    class ShowInSettingsMenuTest extends TestCase
+    {
+        public function testHandleAddsSettingsRouteWhenModuleEnabledAndAccessAllowed(): void
+        {
+            $menu = new class () {
+                /** @var array<int, array<string, mixed>> */
+                public array $calls = [];
+
+                /**
+                 * @param array<int, mixed> $params
+                 * @param array<string, mixed> $options
+                 */
+                public function route(string $route, string $title, array $params, int $order, array $options): void
+                {
+                    $this->calls[] = [
+                        'route' => $route,
+                        'title' => $title,
+                        'params' => $params,
+                        'order' => $order,
+                        'options' => $options,
+                    ];
+                }
+            };
+
+            $listener = new class () extends ShowInSettingsMenu {
+                public function moduleIsEnabled(string $alias): bool
+                {
+                    return true;
+                }
+
+                public function canAccessMenuItem(string $title, string $permission): bool
+                {
+                    return true;
+                }
+            };
+
+            $listener->handle(new SettingsCreated($menu));
+
+            self::assertCount(1, $menu->calls);
+            self::assertSame('nfse.settings.edit', $menu->calls[0]['route']);
+            self::assertSame(260, $menu->calls[0]['order']);
+            self::assertSame('receipt_long', $menu->calls[0]['options']['icon']);
+        }
+
+        public function testHandleSkipsWhenModuleDisabled(): void
+        {
+            $menu = new class () {
+                /** @var array<int, array<string, mixed>> */
+                public array $calls = [];
+
+                public function route(string $route, string $title, array $params, int $order, array $options): void
+                {
+                    $this->calls[] = [];
+                }
+            };
+
+            $listener = new class () extends ShowInSettingsMenu {
+                public function moduleIsEnabled(string $alias): bool
+                {
+                    return false;
+                }
+            };
+
+            $listener->handle(new SettingsCreated($menu));
+
+            self::assertCount(0, $menu->calls);
+        }
+
+        public function testHandleSkipsWhenUserCannotAccessMenuItem(): void
+        {
+            $menu = new class () {
+                /** @var array<int, array<string, mixed>> */
+                public array $calls = [];
+
+                public function route(string $route, string $title, array $params, int $order, array $options): void
+                {
+                    $this->calls[] = [];
+                }
+            };
+
+            $listener = new class () extends ShowInSettingsMenu {
+                public function moduleIsEnabled(string $alias): bool
+                {
+                    return true;
+                }
+
+                public function canAccessMenuItem(string $title, string $permission): bool
+                {
+                    return false;
+                }
+            };
+
+            $listener->handle(new SettingsCreated($menu));
+
+            self::assertCount(0, $menu->calls);
+        }
+    }
+}
