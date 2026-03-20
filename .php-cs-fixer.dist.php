33591cdf503433314fe17502af511638bf7404be
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-20 11:09:46 -0300

    chore: add PHP-CS-Fixer configuration
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,32 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/Http',
+        __DIR__ . '/Models',
+        __DIR__ . '/Providers',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php');
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@PHP82Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'strict_param' => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder($finder);
