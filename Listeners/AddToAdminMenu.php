57d98a0ec97cf2846d9eb2a3e46432bdac8fd559
Vitor Mattos <vitor@php.rio>
2026-03-21 22:19:40 -0300

    feat(menu): add nfse dashboard admin entry
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ Listeners/AddToAdminMenu.php
@@ -0,0 +1,42 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Modules\Nfse\Listeners;
+
+use App\Events\Menu\AdminCreated as Event;
+use App\Traits\Modules;
+use App\Traits\Permissions;
+
+class AddToAdminMenu
+{
+    use Modules;
+    use Permissions;
+
+    public function handle(Event $event): void
+    {
+        if (!$this->moduleIsEnabled('nfse')) {
+            return;
+        }
+
+        $title = trans('nfse::general.name');
+
+        if (!$this->canAccessMenuItem($title, 'read-settings-company')) {
+            return;
+        }
+
+        $event->menu->route(
+            'nfse.dashboard.index',
+            trans('nfse::general.dashboard.menu_title'),
+            [],
+            45,
+            [
+                'icon' => 'receipt_long',
+                'search_keywords' => trans('nfse::general.description'),
+            ]
+        );
+    }
+}
